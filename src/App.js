9b8407d44d04ffdddcd7668ab8f1a2ca73cb474e
Prashant Bhati <prashantbsmvit070@gmail.com>
2023-12-26 21:33:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
+
 
 function App() {
+
+  const [resourceType,setResourcetype]=useState('post')
+
+  useEffect(()=>{
+    console.log("Useeffect is called")
+
+    return ()=>{
+      console.log("Component  is about to  unmount ");
+    }
+  },[resourceType])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{resourceType}</div>
+      <button onClick={()=>setResourcetype('post')}>Post</button>
+      <button onClick={()=>setResourcetype('User')}>User</button>
+      <button onClick={()=>setResourcetype('comment')}>Comment</button>
+      
     </div>
   );
 }
